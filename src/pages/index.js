d24373b784f835281370f26ec937e2e8223f850c
Tharinda98 <tharindathamaranga98@gmail.com>
2021-10-20 19:16:49 +0530

    added routes in index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,6 +20,18 @@ const Pages=()=>{
     return(
         <Router>
             <Route path="/AdminHomePage" component={AdminHomePage} />
+            <Route path="/AdminRegList" component={AdminRegList} />
+            <Route path="/AdminUserList" component={AdminUserList} />
+            <Route path="/AdminRegView" component={AdminRegView} />
+            <Route path="/CustomerHomePage" component={CustomerHomePage} />
+            <Route path="/CustomerSPView" component={CustomerSPView} />
+            <Route path="/CustomerWorkProgress" component={CustomerWorkProgress} />
+            <Route path="/CustomerWorkDetails" component={CustomerWorkDetails} />
+            <Route path="/CustomerWorkDetailsUnpaid" component={CustomerWorkDetailsUnpaid} />
+            <Route path="/review" component={Review} />
+            <Route path="/request" component={WorkRequest} />
+            <Route path="/addWork" component={AddWork} />
+            <Route path="/work" component={Work} />
         </Router>
     );
 };
