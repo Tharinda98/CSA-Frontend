da688c7c86f9bdd3fdc2b1efe9e9c7a9fadd40e0
Tharinda98 <tharindathamaranga98@gmail.com>
2021-10-20 15:23:50 +0530

    App.js file has added Apollo Server

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+//need to be moved to pages
 import { BrowserRouter,Route, Switch } from "react-router-dom";
 import AdminHomePage from "./pages/AdminHomePage";
 import AdminUserList from "./pages/AdminUserList";
@@ -7,65 +8,39 @@ import CustomerHomePage from "./pages/CustomerHomePage";
 import CustomerSPView from "./pages/CustomerSPView";
 import CustomerWorkProgress from "./pages/CustomerWorkProgress";
 import CustomerWorkDetails from "./pages/CustomerWorkDetails";
-
-
 import Review from "./pages/rating";
 import WorkRequest from "./pages/addRequest";
 import AddWork from "./pages/addWork";
 import Work from "./pages/work";
 import CustomerWorkDetailsUnpaid from "./pages/CustomerWorkDetailsUnpaid";
 
+//importing react 
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-function App() {
+//importing Apollo Client Libraries
+import {ApolloClient, ApolloProvider, InMemoryCache} from '@apollo/client';
+
+//have to import pages
+
+//configure out API URI & cache
+const uri =process.env.API_URI;
+const cache = new InMemoryCache();
+
+//configure Apollo Client
+const client= new ApolloClient({
+  uri,
+  cache,
+  connectToDevTools: true
+})
+
+const App=()=> {
   return (
-      <div>
-        
-        <BrowserRouter>
-        <Switch>
-          <Route exact path='/AdminHomePage'>
-              <AdminHomePage/>
-          </Route>
-          <Route exact path='/AdminRegList'>
-            <AdminRegList/>
-          </Route>
-          <Route exact path='/AdminUserList'>
-            <AdminUserList/>
-          </Route>
-          <Route exact path='/AdminRegView'>
-            <AdminRegView/>
-          </Route>
-          <Route exact path='/CustomerHomePage'>
-              <CustomerHomePage/>
-          </Route>
-          <Route exact path='/CustomerSPView'>
-              <CustomerSPView/>
-          </Route>
-          <Route exact path='/CustomerWorkProgress'>
-              <CustomerWorkProgress/>
-          </Route>
-          <Route exact path='/CustomerWorkDetails'>
-              <CustomerWorkDetails/>
-          </Route>
-          <Route exact path='/CustomerWorkDetailsUnpaid'>
-              <CustomerWorkDetailsUnpaid/>
-          </Route>
+      <ApolloProvider client={client}>
 
-          <Route exact path='/review'>
-              <Review/>
-          </Route>
-          <Route exact path='/request'>
-              <WorkRequest/>
-          </Route>
-          <Route exact path='/addWork'>
-              <AddWork/>
-          </Route>
-          <Route exact path='/work'>
-              <Work/>
-          </Route>
-          
-        </Switch>
-        </BrowserRouter>
-      </div>
+        <Pages/>
+
+      </ApolloProvider>
   );
 }
 
